42804171698e87fedcbfa8fa725df0031c9d9980
Luu Huyen <91403067+luhuyn@users.noreply.github.com>
2022-04-22 17:20:29 +0700

    upload display ddmmyy ex

---
+++ ddmmyy/script.js
@@ -0,0 +1,4 @@
+let day = prompt("Input day you want to display: ");
+let month = prompt("Input month you want to display: ");
+let year = prompt("Input year you want to display: ");
+document.write(day + '-' + month + '-' + year);
