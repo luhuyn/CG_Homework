9d1eb625710a06e63e6229ec9542e42e12d71ff6
Luu Huyen <91403067+luhuyn@users.noreply.github.com>
2022-04-22 17:35:06 +0700

    upload perimeter&area circle ex

---
+++ perimeter&area/script.js
@@ -0,0 +1,6 @@
+let r = prompt("Input radious: ");
+let c = 2 * r * Math.PI;
+let s = Math.pow(r,2) * Math.PI;
+document.write("Perimeter: 2 x 3.14 x " + r + '= ' + c);
+document.write('<br/>');
+document.write("Area: 3.14 x " + r +'^2' + '= ' + s);
