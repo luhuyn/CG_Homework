8feb369a7d60715aae0131aba1ffa048d51fb463
Lưu Huyền <91403067+luhuyn@users.noreply.github.com>
2022-05-16 17:56:34 +0700

    Delete ddmmyy directory

--- ddmmyy/script.js
+++
@@ -1,4 +0,0 @@
-let day = prompt("Input day you want to display: ");
-let month = prompt("Input month you want to display: ");
-let year = prompt("Input year you want to display: ");
-document.write(day + '-' + month + '-' + year);
